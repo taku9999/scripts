b19edb4f14e646c39a42030c45c752de69bf6462
taku9999 <102945088+taku9999@users.noreply.github.com>
2023-04-28 08:43:11 +0000

    [update] 保存設定を拡張

--- point_sub.py
+++ point_sub.py
@@ -9,13 +9,19 @@ import ros_numpy
 import open3d as o3d
 import time
 import datetime
+import os
 
-filepass = "/workspace/docker_bind_Local/lidar"
+dirpass = "/workspace/docker_bind_Local"
 
 def get_time():
     now_time = datetime.datetime.now() + datetime.timedelta(hours=9)
     time_str = str(now_time.strftime("%Y-%m-%d_%H:%M:%S_%f")[:-4])
     return time_str
+
+def make_dirname():
+    global dirpass
+    dirpass += "/lidar-data/" + get_time()
+    os.makedirs(dirpass)
 
 def lidar_callback(message):
     print("========== PCD data receive ==========")
@@ -26,25 +32,24 @@ def lidar_callback(message):
     points[:,2]=pc['z']
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(np.array(points, dtype=np.float32))
-    o3d.io.write_point_cloud(filepass + "/" + get_time() +".pcd", pcd)
+    o3d.io.write_point_cloud(dirpass + "/" + get_time() +".pcd", pcd)
 
 def imu_callback(imu_data):
     print("========== IMU Data receive ==========")
-    with open(filepass + "/imu_log.csv", mode="a") as f:
+    with open(dirpass + "/imu_log.csv", mode="a") as f:
         f.write(get_time())
         f.write("," + str(imu_data.angular_velocity.x) + "," + str(imu_data.angular_velocity.y) + "," + str(imu_data.angular_velocity.z))
         f.write("," + str(imu_data.linear_acceleration.x) + "," + str(imu_data.linear_acceleration.y) + "," + str(imu_data.linear_acceleration.z) + "\r\n")
     # time.sleep(5)
 
 def subscriber():
-    #ノードの初期化
-    rospy.init_node('point_sub') 
-    #点群トピックSubscribe用
-    rospy.Subscriber('/livox/lidar', PointCloud2 , lidar_callback)
-    #IMUトピックSubscribe用
-    rospy.Subscriber('/livox/imu', Imu , imu_callback)
-    #コールバック関数を繰り返し呼び出す
-    rospy.spin()
+    rospy.init_node('point_sub') #ノードの初期化
+    rospy.Subscriber('/livox/lidar', PointCloud2 , lidar_callback)  #点群トピックSubscribe用
+    rospy.Subscriber('/livox/imu', Imu , imu_callback) #IMUトピックSubscribe用
+
+    make_dirname() # データ保存先の設定
+
+    rospy.spin() #コールバック関数を繰り返し呼び出す
 
 if __name__ == "__main__":
     subscriber()
